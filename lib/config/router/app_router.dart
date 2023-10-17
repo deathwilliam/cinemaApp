810f9f1284d0fc9e44ee1360c029ebf7b6397470
Wilfredo <melgar.wilfredo@gmail.com>
2023-10-17 08:58:03 -0600

    first commit

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,29 @@
+import 'package:go_router/go_router.dart';
+
+import 'package:cinemapedia/presentation/screens/screens.dart';
+
+final appRouter = GoRouter(
+  initialLocation: '/',
+  routes: [
+    
+    GoRoute(
+      path: '/',
+      name: HomeScreen.name,
+      builder: (context, state) => const HomeScreen(),
+      routes: [
+         GoRoute(
+          path: 'movie/:id',
+          name: MovieScreen.name,
+          builder: (context, state) {
+            final movieId = state.params['id'] ?? 'no-id';
+
+            return MovieScreen( movieId: movieId );
+          },
+        ),
+      ]
+    ),
+
+
+
+  ]
+);
---
+++ lib/domain/repositories/movies_repository.dart
@@ -0,0 +1,17 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+
+
+abstract class MoviesRepository {
+
+  Future<List<Movie>> getNowPlaying({ int page = 1 });
+
+  Future<List<Movie>> getPopular({ int page = 1 });
+  
+  Future<List<Movie>> getUpcoming({ int page = 1 });
+
+  Future<List<Movie>> getTopRated({ int page = 1 });
+
+  Future<Movie> getMovieById( String id );
+
+
+}
